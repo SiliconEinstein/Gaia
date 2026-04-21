745e471fe8d97a22f3cb97c48110a9c525d64330
kunchen <chenkun0228@gmail.com>
2026-04-21 20:34:16 +0800

    feat(lang): generate review manifests for v6 actions

---
+++ gaia/lang/review/__init__.py
@@ -0,0 +1,6 @@
+"""Review helpers for Gaia Lang v6."""
+
+from gaia.lang.review.manifest import generate_review_manifest
+from gaia.lang.review.templates import generate_audit_question
+
+__all__ = ["generate_audit_question", "generate_review_manifest"]
